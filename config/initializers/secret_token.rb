9b9853bdd03f551a943da5197490bdf006696ba6
ptotem <info@ptotem.com>
2012-05-25 17:37:03 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PtotemWebSite::Application.config.secret_token = 'eac2c975be452a48a76d1a3a6413da0dd7072b4dbf2745ea911a9951f3fb7c95a55c12b83d0e6fd303a35547f853db50ecf256270d0d1e56c6e5b007b7887edd'
